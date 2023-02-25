1bca1f1b8bbbbc7c11b9fce42c86ece09419bb5b
AiP24 <aipacifico@s.sfusd.edu>
2023-02-25 12:25:57 -0800

    add smart bluealliance response caching

--- scoutingbackend/__init__.py
+++ scoutingbackend/__init__.py
@@ -15,13 +15,16 @@ def create_app():
     app = flask.Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=str(pathlib.Path(app.instance_path, 'scoutingdb.sqlite'))
+        DATABASE=str(pathlib.Path(app.instance_path, 'scoutingdb.sqlite')),
+        MANUAL_CACHE=str(pathlib.Path(app.instance_path, 'manual_cache'))
     )
     app.config.from_pyfile(str(pathlib.Path(app.instance_path, 'config.py')))
     flask_cors.CORS()
     database.db.connect(app.config['DATABASE'])
     
     with app.app_context():
+        bluealliance.session.set_manual_cache(app.config['MANUAL_CACHE'])
+        
         a = api.Api()
         ba = bluealliance.BlueAlliance(app.config['TBA_KEY'])
         ba.register(a.bp)
--- scoutingbackend/routes/bluealliance.py
+++ scoutingbackend/routes/bluealliance.py
@@ -1,16 +1,65 @@
 import contextvars
 import datetime
+import time
+import urllib.parse
 import json
+import os
+import pathlib
 import sqlite3  # typing only
 
 import flask
+import werkzeug.datastructures
 import flask_restful
 import requests
 
 from .. import schemes
-from ..database import db
+from .. import database
 
-session = requests.Session() #SHOULD REALLY BE A CONTEXT-SENSITIVE VARIABLE BUT WHATEVER
+class CachingSession(requests.Session):
+    def __init__(self, manual_cache: os.PathLike | str | None = None) -> None:
+        super().__init__()
+        if manual_cache:
+            self.cache_path = pathlib.Path(manual_cache)
+        else:
+            self.cache_path = None
+    
+    def set_manual_cache(self, manual_cache: os.PathLike | str):
+        self.cache_path = pathlib.Path(manual_cache)
+    
+    def generate_response(self, data: bytes, code: int = 200):
+        '''Generates a semi-fake requests response'''
+        resp = requests.Response()
+        resp.status_code = code
+        resp._content = data
+        return resp
+    
+    def get(self, url: str, cache_control: werkzeug.datastructures.RequestCacheControl | None = None, **kwargs) -> requests.Response:
+        parsed_url = urllib.parse.urlparse(url)
+        if 'thebluealliance.com' not in parsed_url.netloc: #not BA
+            return super().get(url, **kwargs)
+        if not self.cache_path or not cache_control: #cache not enabled #DO NOT MERGE INTO THE TOP STATEMENT, IT IS SPLIT FOR READABILITY
+            return super().get(url, **kwargs)
+        path_list = parsed_url.path.split("/")[3:]
+        cache_path = pathlib.Path(self.cache_path, *path_list[:-1], f"{path_list[-1]}.json")
+        if cache_path.exists() and not cache_control.no_cache:
+            cache_json = json.loads(cache_path.read_text())
+            real_data = json.dumps(cache_json['data']).encode('utf8')
+            if 'last-update' in cache_json and (cache_control.max_age is None or time.time() - cache_json['last-update'] <= cache_control.max_age):
+                return self.generate_response(real_data, code=cache_json['code'] if 'code' in cache_json else 200)
+            #if the response isn't new enough let the if-statement fall back to manually requesting and caching
+        resp = super().get(url, **kwargs)
+        if cache_control.no_store or not self.cache_path.is_dir():
+            return resp
+        cached_data = {
+            "last-update": time.time(),
+            "code": resp.status_code,
+            "data": resp.json()
+        }
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+        cache_path.write_text(json.dumps(cached_data))
+        return resp
+
+session = CachingSession() #SHOULD REALLY BE A CONTEXT-SENSITIVE VARIABLE BUT WHATEVER
 
 class BlueAlliance(object):
     def __init__(self, api_key: str) -> None:
@@ -36,16 +85,17 @@ class BlueAlliance(object):
     
     class BAIndex(flask_restful.Resource):
         def get(self):
-            resp = session.get("https://www.thebluealliance.com/api/v3/status")
+            resp = session.get("https://www.thebluealliance.com/api/v3/status", cache_control=flask.request.cache_control)
             if resp.status_code != 200:
                 return flask_restful.abort(resp.status_code)
             respj = resp.json()
+            open("TEST.json", 'wb+').write(resp.content)
             return {"max_season": respj['max_season'], "current_season": respj['current_season']}
             
     class BASeason(flask_restful.Resource):
         def get(self, season: int):
             ignore_date = flask.request.args.get('ignoreDate', "false").lower()=="true"
-            resp = session.get(f"https://www.thebluealliance.com/api/v3/events/{season}/simple")
+            resp = session.get(f"https://www.thebluealliance.com/api/v3/events/{season}/simple", cache_control=flask.request.cache_control)
             if resp.status_code != 200:
                 return flask_restful.abort(resp.status_code)
             j = resp.json()
@@ -53,7 +103,7 @@ class BlueAlliance(object):
     
     class BAEvent(flask_restful.Resource):
         def get(self, season: int, event: str):
-            resp = session.get(f"https://www.thebluealliance.com/api/v3/event/{season}{event}/matches/simple")
+            resp = session.get(f"https://www.thebluealliance.com/api/v3/event/{season}{event}/matches/simple", cache_control=flask.request.cache_control)
             if resp.status_code != 200:
                 return flask_restful.abort(resp.status_code)
             j = resp.json()
@@ -62,7 +112,7 @@ class BlueAlliance(object):
     class BAMatch(flask_restful.Resource):
         def get(self, season: int, event: str, match: str):
             match_code = f"{season}{event}_{match}"
-            resp = session.get(f"https://www.thebluealliance.com/api/v3/match/{match_code}/simple")
+            resp = session.get(f"https://www.thebluealliance.com/api/v3/match/{match_code}/simple", cache_control=flask.request.cache_control)
             if resp.status_code != 200:
                 return flask_restful.abort(resp.status_code)
             j = resp.json()
