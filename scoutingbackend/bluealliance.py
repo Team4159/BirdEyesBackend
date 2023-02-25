584f0abaf92ed42e2b36ee94ee02603323955da2
yuenjo <yuenjo06@gmail.com>
2023-02-24 20:44:47 -0800

    add asterisk match endpoint

--- scoutingbackend/bluealliance.py
+++ scoutingbackend/bluealliance.py
@@ -5,31 +5,35 @@ from flask import Blueprint, Response, abort
 bp = Blueprint('bluealliance', __name__, url_prefix='/bluealliance')
 
 def init_cache(app):
-    global season, event, matches, teams
+    global _season, _event, _matches, _teams
     with app.app_context():
         jo = json.load(open(os.path.join(app.instance_path, 'cache.json'), "r"))
         if "season" not in jo or "event" not in jo or "matches" not in jo or "teams" not in jo:
             raise Exception("Malformed JSON Cache")
-        season = jo["season"]
-        event = jo["event"]
-        matches = jo["matches"]
-        teams = jo["teams"]
+        _season = jo["season"]
+        _event = jo["event"]
+        _matches = jo["matches"]
+        _teams = jo["teams"]
+        _teams["*"] = {}
+        for vo in _teams.values():
+            for k in vo.keys():
+                _teams["*"][k] = "*"
 
 @bp.route("/", methods=("GET",))
 def current_seasons():
-    return {"max_season": season, "current_season": season}
+    return {"max_season": _season, "current_season": _season}
 
 @bp.route('/<season>/', methods=("GET",))
 def current_events(season):
-    if (season != str(season)): return abort(Response("Response Not Cached", 404))
-    return {event: "Cached Event"}
+    if (season != str(_season)): return abort(Response("Response Not Cached", 404))
+    return {_event: "Cached Event"}
 
 @bp.route('/<season>/<event>/', methods=("GET",))
 def current_matches(season, event):
-    if (season != str(season) or event != event): return abort(Response("Response Not Cached", 404))
-    return matches
+    if (season != str(_season) or event != _event): return abort(Response("Response Not Cached", 404))
+    return _matches
 
 @bp.route('/<season>/<event>/<match>/', methods=("GET",))
 def match_info(season, event, match):
-    if (season != str(season) or event != event or match not in teams): return abort(Response("Response Not Cached", 404))
-    return teams[match]
+    if (season != str(_season) or event != _event or match not in _teams): return abort(Response("Response Not Cached", 404))
+    return _teams[match]
