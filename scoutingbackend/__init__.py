69f494975d3039383f2731f718d7f2e767c5fc6f
AiP24 <aipacifico@s.sfusd.edu>
2023-03-21 10:20:24 -0700

    point plot graph

--- scoutingbackend/__init__.py
+++ scoutingbackend/__init__.py
@@ -21,19 +21,20 @@ def create_app():
     
     with app.app_context():
         bluealliance.session.set_manual_cache(app.config['MANUAL_CACHE'])
-        app.context_processor
+        #app.context_processor
         a = api.Api()
         ba = bluealliance.BlueAlliance(app.config["TBA_KEY"])
         ba.register(a.bp) # /api/bluealliance
         an = analysis.Analysis2023()
         an.register(a.bp) # /api/analysis
         g = graphics.Graphics2023(app.config['MANUAL_CACHE'])
-        g.register(an.bp) # /api/analysis/graphics
+        g.register(a.bp) # /api/graphics
         a.register(app) # /api/
 
         # if you want to access bluealliance functions (WARNING: may work slightly funny), you can now use current_app.api/current_app.bluealliance.xyz
         app.api: api.Api = a #type:ignore
         app.bluealliance: bluealliance.BlueAlliance = ba #type:ignore
+        app.gr = g
     
     pathlib.Path(app.instance_path).mkdir(parents=True, exist_ok=True)
     
--- scoutingbackend/routes/graphics.py
+++ scoutingbackend/routes/graphics.py
@@ -1,13 +1,17 @@
+import io
 import itertools
 import os
 import pathlib
 import typing
+import matplotlib.pyplot
 
 import flask
 import flask_restful
 
 from scoutingbackend.database import db
+from scoutingbackend.routes.analysis import total_points
 
+matplotlib.pyplot.ioff()
 
 class Graphics2023(object):
     cache: typing.Optional[pathlib.Path] = None
@@ -17,6 +21,7 @@ class Graphics2023(object):
         self.bp = flask.Blueprint('graphics', __name__, url_prefix='/graphics')
         self.rest = flask_restful.Api(self.bp)
         self.rest.add_resource(self.JojoWheel, '/<int:year>/<string:event>/<int:team>/wheel')
+        self.rest.add_resource(self.StatGraph, '/<int:year>/<string:event>/<int:team>/pointgraph')
 
     def register(self, app: typing.Union[flask.Flask, flask.Blueprint]):
         app.register_blueprint(self.bp)
@@ -39,3 +44,35 @@ class Graphics2023(object):
                 all_miss = sum(sum(e[k] for k in keylist if k.endswith('misses')) for e in teaminfo)
                 all_ok = sum(sum(e[k] for k in keylist if (not k.endswith('misses'))) for e in teaminfo)
                 print(all_miss, all_ok)
+    
+    class StatGraph(flask_restful.Resource):
+        def get(self, year: int, event: str, team: int):
+            raw_data = db.connection().cursor().execute(f"SELECT * FROM frc{year}{event}_match WHERE teamNumber={team}").fetchall()
+            matchids = []
+            autopts = []
+            telepts = []
+            endgpts = []
+            for match in raw_data:
+                matchids.append(match['match'])
+                totalauto = total_points(match, 'auto')
+                totalteleop = total_points(match, 'teleop')
+                totalendgame = total_points(match, 'endgame')
+                if totalauto is None: #unlikely but possible
+                    totalauto = 0
+                if totalteleop is None:
+                    totalteleop = 0
+                if totalendgame is None:
+                    totalendgame = 0
+                autopts.append(totalauto)
+                telepts.append(totalteleop)
+                endgpts.append(totalendgame)
+            matplotlib.pyplot.cla()
+            matplotlib.pyplot.title(f"Team {team} Points")
+            matplotlib.pyplot.xlabel("Match")
+            matplotlib.pyplot.ylabel("Points")
+            matplotlib.pyplot.stackplot(matchids, autopts, telepts, endgpts, labels=["Auto", "Teleop", "Endgame"])
+            matplotlib.pyplot.legend(loc="upper left")
+            out = io.BytesIO()
+            matplotlib.pyplot.savefig(out, format='png')
+            out.seek(0)
+            return flask.Response(out.read(), 200, content_type='image/png')
